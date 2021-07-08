c17de193fc8fe232265bad66392ef041c69afa3c
Kemayou Kévin <kemayou.ke@gmail.com>
2021-07-08 14:54:26 +0200

    Starting the 2nd step

--- API/server.js
+++ API/server.js
@@ -1,20 +1,42 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
-const countriesData = require("../countriesData.json");
+const countriesData = require("./countriesData.json");
 const cors = require("cors");
 app.use(cors());
-let countriesNames = []
+/* let countriesNames = []
 for(let i = 0; i < countriesData.length; i ++){
     countriesNames.push(countriesData[i].name)
-}
+} */
 
 
 
 app.get('/all', (req, res) => {
     res.json({
-        data: countriesNames
+        data: countriesData
     });
+})
+
+app.get("/:countryName", (req, res) => {
+    const countryName = req.params.countryName.toLowerCase();
+
+    /* On filtre les données des pays pour récupérer juste l'objet contenant le pays qui nous intéresse */
+    const countryData = countriesData.filter((country) => country.name.toLowerCase() === countryName)
+
+    res.json({
+        data: countryData,
+    })
+})
+
+app.get("/:countryCapital", (req, res) => {
+    const countryCapital = req.params.countryCapital.toLowerCase();
+
+    /* On filtre les données des pays pour récupérer juste l'objet contenant la capital qui nous intéresse */
+    const countryData = countriesData.filter((country) => country.capital.toLowerCase() === countryCapital);
+
+    res.json({
+        data: countryData,
+    })
 })
 
 
---
+++ front/js/script.js
@@ -0,0 +1,98 @@
+$(() => {
+    // Au démarrage du site, on récupère la liste de tous les pays
+	// et on "branche" une fonction au click du bouton
+    getAllCountries();
+
+    $("#btnShowData").click(handleClick);
+});
+
+const getAllCountries = async () => {
+    const allCountries = await fetch("http://localhost:8000/all");
+    // On transforme la réponse en objet JSON utilisable dans notre code JS
+    const countries = await allCountries.json();
+    console.log(countries);
+    updateList(countries.data);
+
+}
+
+// Fonction qui permet de mettre à jour la liste en injectant du HTML
+// dans cette dernière
+const updateList = (list) => {
+    // Effacer la liste existante 
+    $("#countryList").empty();
+
+    // Mise à jour de la liste à l'écran 
+    list.forEach((country) => {
+        $("#countryList").append(
+            `
+            <li>
+                <p>${country.name} - ${country.capital}</p>
+            </li>
+            `
+        ) 
+
+    }); 
+
+}
+
+
+// Fonction qui effectue une requête au backend quand on clique
+// sur le bouton Show Data en récupérant la valeur de l'input
+// entrée par l'utilisateur
+const handleClick = async () => {
+    const userSearchValue = $("#inputValue").val();
+    console.log(userSearchValue);
+    const res = await fetch(`http://localhost:8000/${userSearchValue}`);
+    const jsonRes = await res.json();
+    console.log(jsonRes);
+
+    updateList(jsonRes.data);
+}
+
+
+
+/* $(() => {
+    getInitialData();
+
+    $("button").click(handleClick)
+});
+
+async function getInitialData() {
+    const res = await fetch("http://localhost:8000/all");
+    const jsonRes = await res.json();
+
+    updateList(jsonRes.data);
+}
+
+function updateList(list) {
+    // Effacer la list existante 
+
+    $("#list").empty();
+
+    // Mise à jour de la liste à l'écran
+    list.forEach(country) =>
+    $("#list").append(
+        `
+        <li>
+            <p>${country.name} - ${country.capital}</p>
+        </li>
+        `
+        ) 
+    )
+}
+
+
+async function handleClick() {
+    const userSearchValue = $("input").val();
+    console.log(userSearchValue);
+    const res = await fetch(`http://localhost:8000/${userSearchValue}`);
+    const resJson = await res.json();
+
+    updateList(jsonRes.data);
+
+}
+*/
+
+
+
+
